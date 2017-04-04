4a76715e793b05eaff095a37fe7d7f67a851bbc5
peterormai <ormaipeter.fx@gmail.com>
2017-04-04 15:17:50 +0200

    export done

--- export.py
+++ export.py
@@ -1,13 +1,16 @@
+import reports
 
-# Export functions
+
+def export_answers(filename="answers.txt"):
+    with open(filename, 'w') as file:
+        file.write(str(reports.count_games('game_stat.txt')) + "\n" +
+                   str(reports.decide('game_stat.txt', 1999)) + "\n" +
+                   reports.get_latest('game_stat.txt') + "\n" +
+                   str(reports.count_by_genre('game_stat.txt', 'Survival game')) + "\n" +
+                   str(reports.get_line_number_by_title('game_stat.txt', 'World of Warcraft')) + "\n" +
+                   str(reports.sort_abc('game_stat.txt')) + "\n" +
+                   str(reports.get_genres('game_stat.txt')) + "\n" +
+                   str(reports.when_was_top_sold_fps('game_stat.txt')))
 
 
-def export_inventory(inventory, filename="export_inventory.csv"):
-    file = open(filename, 'w')
-    for i in inventory:
-        file.write((i + ',') * inventory[i])
-    file.close()
-    import os
-    with open(filename, 'rb+') as filehandle:
-        filehandle.seek(-1, os.SEEK_END)
-        filehandle.truncate()
+export_answers()
--- printing.py
+++ printing.py
@@ -1,17 +1,20 @@
 import reports
+import pprint
 
-print(reports.count_games('game_stat.txt'))
+pp = pprint.PrettyPrinter(indent=1, width=150, depth=None, stream=None)
 
-print(reports.decide('game_stat.txt', 1999))
+pp.pprint(reports.count_games('game_stat.txt'))
 
-print(reports.get_latest('game_stat.txt'))
+pp.pprint(reports.decide('game_stat.txt', 1999))
 
-print(reports.count_by_genre('game_stat.txt', 'Survival game'))
+pp.pprint(reports.get_latest('game_stat.txt'))
 
-print(reports.get_line_number_by_title('game_stat.txt', 'World of Warcraft'))
+pp.pprint(reports.count_by_genre('game_stat.txt', 'Survival game'))
 
-print(reports.sort_abc('game_stat.txt'))
+pp.pprint(reports.get_line_number_by_title('game_stat.txt', 'World of Warcraft'))
 
-print(reports.get_genres('game_stat.txt'))
+pp.pprint(reports.sort_abc('game_stat.txt'))
 
-print(reports.when_was_top_sold_fps('game_stat.txt'))
+pp.pprint(reports.get_genres('game_stat.txt'))
+
+pp.pprint(reports.when_was_top_sold_fps('game_stat.txt'))
